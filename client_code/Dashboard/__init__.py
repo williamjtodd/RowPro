695e51d1dbe868304987ecb0809fb173e1edef90
s1024162@haileybury.vic.edu.au <s1024162@haileybury.vic.edu.au>
2024-06-10 23:58:12 +0000

    Created Dashboard.ItemTemplate1, Dashboard and Dashboard.ItemTemplate2; Edited settings; Deleted Form1

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import DashboardTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class Dashboard(DashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
