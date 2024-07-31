df668d8ee718560fa462bc3f88a0cfc0c69c47fe
s1024162@haileybury.vic.edu.au <s1024162@haileybury.vic.edu.au>
2024-07-31 09:00:29 +0000

    Created LICENSE.txt; Edited dataAPI, formDashboard, formDashboard.ItemTemplate1, formImport and 5 more

--- client_code/formDashboard/__init__.py
+++ client_code/formDashboard/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import formDashboardTemplate
 from anvil import *
+import anvil.users
 import anvil.server
 import plotly.graph_objects as go
 import anvil.tables as tables
--- client_code/formProfile/__init__.py
+++ client_code/formProfile/__init__.py
@@ -5,7 +5,6 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class formProfile(formProfileTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
